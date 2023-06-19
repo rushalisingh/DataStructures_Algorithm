7577462bc33de5f7c2b498cb3d83f030e0653388
RUSHALI RANJAN SINGH <101826211+rushalisingh@users.noreply.github.com>
2023-06-19 20:44:44 +0530

    Add files via upload

---
+++ TRIES/TrieImplementation.java
@@ -0,0 +1,55 @@
+public class TrieImplementation {
+
+    //node class
+    static class Node{
+        //2 variables
+        Node [] children = new Node[26];
+        boolean eow = false;
+        Node(){
+            for(int i=0;i<26;i++)
+            {
+                children[i]=null;
+            }
+        }
+    }
+    //root initialize
+    public static Node root = new Node();
+    //insert func
+    public static void insert(String word)
+    {
+        Node curr =root;
+        for(int i=0 ;i<word.length() ;i++)
+        {
+            int idx= word.charAt(i)-'a';
+            if(curr.children[idx]==null)
+            {
+                curr.children[idx]= new Node();
+            }
+            curr=curr.children[idx];
+        }
+        curr.eow =true;
+    }
+    //search func
+    public static boolean search(String key)
+    {
+        Node curr =root;
+        for(int i=0;i<key.length();i++)
+        {
+            int idx =key.charAt(i)-'a';
+            if(curr.children[idx]==null)
+            {
+                return false;
+            }
+            curr =curr.children[idx];
+        }
+        return curr.eow =true;
+    }
+
+    public static void main(String[] args) {
+        String word[] = {"trie" ,"data" ,"structure"};
+        for(int i=0;i<word.length;i++)
+        {
+            insert(word[i]);
+        }
+    }
+}
